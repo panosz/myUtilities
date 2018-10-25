cdea4e7a088f48f2ea8bab280195eafd27506fc6
Panagiotis Zestanakis <panosz@gmail.com>
2018-10-25 16:16:55 +0300

    Version 2.0.0-beta. Breaking Change.  Clients should #include "myUtilities/myUtilities.hpp" instead of #include "myUtilities.hpp"
    Users can also include dedicated headers for specific functionality

---
+++ src/myUtilities/src/linspace.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Panagiotis Zestanakis on 25/10/18.
+//
+#include <boost/range/counting_range.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
+#include <boost/range/adaptor/transformed.hpp>
+
+
+#include "linspace.hpp"
+
+namespace PanosUtilities
+{
+
+    std::vector<double> linspace (double begin, double end, size_t numOfsamples)
+    {
+      std::vector<double> output;
+      output.reserve(numOfsamples);
+
+      if (numOfsamples < 2)
+        throw std::domain_error("linspace: number of samples must be greater than 1");
+
+      auto cRange = boost::counting_range(static_cast<size_t >(0), numOfsamples);
+
+      auto transform = [begin, end, numOfsamples] (int i)
+      { return begin + i * (end - begin) / (numOfsamples - 1); };
+
+      boost::push_back(output, cRange | boost::adaptors::transformed(transform));
+
+      return output;
+    }
+}
